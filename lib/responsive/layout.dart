f95fbb1a48fdea4b165588c5bb6d4db3abe29db4
AL-Dos <barbarianclaw1@gmail.com>
2024-10-04 14:48:36 +0800

    added responsive design on devices

---
+++ lib/responsive/layout.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget androidBody;
+  final Widget webBody;
+
+  const ResponsiveLayout(
+      {super.key, required this.androidBody, required this.webBody});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constrainst) {
+      if (constrainst.maxWidth < 600) {
+        return androidBody;
+      } else {
+        return webBody;
+      }
+    });
+  }
+}
